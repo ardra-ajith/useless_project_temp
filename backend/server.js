bd0e59db7d8440f077c2f06dd034dc8dee7e8c2e
annarajan199 <annarajan199@gmail.com>
2026-09-04 03:13:09 +0530

    final

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,15 @@ const PORT = 3000;
 app.get("/", (req, res) => {
     res.send("Reminder Kozhi backend is working! 🐔");
 });
+app.get("/api/reminder", (req, res) => {
+    const randomIndex = Math.floor(Math.random() * reminders.length);
 
+    const message = reminders[randomIndex];
+
+    res.json({
+        message: message
+    });
+});
 app.listen(PORT, () => {
     console.log(`Server running at http://localhost:${PORT}`);
 });
